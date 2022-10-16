381edbc94b8f5fe4a291d3fa734ad53a3d63f69d
ourystd <amadoury313@gmail.com>
2022-10-16 17:13:32 +0000

    asset management; yarn to pnpm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import join from "lodash/join";
 
-function component() {
-  const element = document.createElement("div");
+import "./style.css";
+import CarImage from "./car.webp";
+
+function components() {
+  const element = document.createElement("h1");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = join(["Hello", "webpack"], " ");
 
-  return element;
-}
+  const img = new Image(500);
+  img.src = CarImage;
 
-document.body.appendChild(component());
+  // element.appendChild(img);
+
+  return [element, img];
+}
+const App = document.createElement("div");
+App.id = "app-root";
+
+App.append(...components());
+
+document.body.appendChild(App);
