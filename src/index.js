2e246137010323c59357e3d73b7c3dd9e6af743f
ourystd <amadoury313@gmail.com>
2022-10-16 03:01:33 +0000

    webpack basic setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import join from "lodash/join";
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
